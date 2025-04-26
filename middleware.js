d60a44e31d7b5d54af71adf3812e8cf32878c5c4
AKASH KUMAR <akash3571kumar@gmail.com>
2025-04-26 13:58:39 +0530

    hehe

--- middleware.js
+++ middleware.js
@@ -1,12 +1,10 @@
 module.exports.isLoggedIn = (req, res, next) => {
-    if (!req.query.userId) {
+    if (!req.params.userId) {
         req.flash("error", "You must be logged in!!");
         return res.redirect("/login");
     }
     next();
 };
-
-
 
 module.exports.setCurrentUser = (req, res, next) => {
     res.locals.currentUser = req.user || null;
