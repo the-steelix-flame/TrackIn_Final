54f28f5153725f9b354e04183812d2ef4ba18129
AKASH KUMAR <akash3571kumar@gmail.com>
2025-04-26 13:50:57 +0530

    Hey I thinkk everything is working

--- middleware.js
+++ middleware.js
@@ -1,12 +1,11 @@
 module.exports.isLoggedIn = (req, res, next) => {
-    // console.log(req.path,req.originalUrl);
-    if (!req.isAuthenticated()) {  //TO_CHECK_USER_IS_LOGGED-IN_OR_NOT!!
-        //req.session.redirectUrl = req.originalUrl;
+    if (!req.user) {  
         req.flash("error", "You must be logged in!!");
         return res.redirect("/login");
     }
     next();
 };
+
 
 module.exports.setCurrentUser = (req, res, next) => {
     res.locals.currentUser = req.user || null;
