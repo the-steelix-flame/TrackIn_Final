f47f644b44f87996c010f3619c91725f5937b426
AKASH KUMAR <akash3571kumar@gmail.com>
2025-05-07 00:40:49 +0530

    check final

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
-    // console.log(req.path,req.originalUrl);
+    console.log(req.path,req.originalUrl);
     // if (!req.isAuthenticated()) {  //TO_CHECK_USER_IS_LOGGED-IN_OR_NOT!!
     //     //req.session.redirectUrl = req.originalUrl;
     //     req.flash("error", "You must be logged in!!");
